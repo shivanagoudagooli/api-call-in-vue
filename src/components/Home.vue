238a8e803b1fc610fce2eb09d9a73e84121557bf
shivanagouda gooli <shivanagoudagooli@gmail.com>
2020-01-08 13:07:22 +0530

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <div v-for="id in res" v-bind:key="id">
+      <child-name :set="id"/>
+    </div>
+  
+</div>
+</template>
+<script>
+import Child from './SingleImage.vue'
+export default {
+    components:{
+      'child-name':Child
+    },
+    data(){
+      return{
+        res:"",
+      }
+    },
+    mounted(){
+        fetch('http://www.splashbase.co/api/v1/images/latest')
+        .then((response) => {
+          return response.json();
+        })
+        .then((myJson) => {
+          this.res = myJson;
+          /*eslint-disable*/
+          console.log(myJson)
+        });
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/SingleImage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div id="main">
+             <div v-bind:key="i" v-for="i in set" >
+                 <img :src="i.url"  width="100px" height="60px" />
+                 <p>{{i.id}}</p>
+             </div>
+               
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:["set"],
+    data(){
+        return{
+            id:"",
+            url:""
+        }
+    },
+   
+}
+</script>
+<style scoped>
+#main {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #c3c3c3;
+  display: -webkit-flex; /* Safari */
+  -webkit-flex-wrap: wrap; /* Safari 6.1+ */
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#main div {
+    margin: 25px;
+  width:100px;
+  height:150px;
+}
+
+</style>
